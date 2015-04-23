27d5fe36cc14aa08434488b0fed87a214eb3393b
Hackbright Student <info@hackbrightacademy.com>
2015-04-23 12:40:10 -0700

    finished ex. flask

---
+++ nice.py
@@ -0,0 +1,92 @@
+from random import choice
+
+from flask import Flask, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+# route to handle the landing page of a website.
+@app.route('/')
+def start_here():
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Hi There!</title>
+        </head>
+        <body>
+            <a href ="/hello">
+                <h1>Hi There!</h1>
+            </a>
+        </body>
+    </html>
+"""
+
+# route to display a simple web page
+@app.route('/hello')
+def say_hello():
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Hi There!</title>
+        </head>
+        <body>
+            <h1>Hi There!</h1>
+            <form action="/greet">
+                <label>What's your name? <input type="text" name="person"></label>
+                <input type="submit">
+                
+                <label> 
+                    <br>
+                    <input type="radio" name="compliment" value="awesome">awesome
+                    <br>
+                    <input type="radio" name="compliment" value="terrific">terrific
+                    <br>
+                    <input type="radio" name="compliment" value="fantastic">fantastic
+                    <br>
+                    <input type="radio" name="compliment" value="neato">neato
+                    <br>
+                    <input type="radio" name="compliment" value="fantabulous">fantabulous
+                    <br>
+                    <input type="radio" name="compliment" value="wowza">wowza
+                    <br>
+                    <input type="radio" name="compliment" value="brilliant">brilliant
+                    <br>
+                </lable>
+
+            </form>
+        </body>
+    </html>
+
+    """
+
+@app.route('/greet')
+def greet_person():
+    player = request.args.get("person")
+    compliment = request.args.get("compliment")
+
+    # AWESOMENESS = [
+    #     'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    #     'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+    # compliment = choice(AWESOMENESS)
+
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>A Compliment</title>
+        </head>
+        <body>
+            Hi %s I think you're %s!
+        </body>
+    </html>""" % (player, compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
